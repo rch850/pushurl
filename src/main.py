27a107f10693debe9cb70ac0af150502f06af908
rch850 <rich850@gmail.com>
2012-02-04 05:35:01 +0900

    Added core functions of pushurl.

--- src/main.py
+++ src/main.py
@@ -2,12 +2,37 @@ import sys
 sys.path.insert(0, "./libs")
 
 from google.appengine.ext.webapp.util import run_wsgi_app
-from flask import Flask
+from flask import (Flask, render_template, request, url_for, make_response,
+                   redirect)
 
 app = Flask(__name__)
+urls = {}
 
 @app.route('/')
-def index():
-    return 'Hello Flask on Google App Engine!'
+def hello_world():
+    return 'Hello World!!!'
+
+@app.route('/<int:page_id>')
+def page(page_id):
+    return render_template('page.html', page_id=page_id)
+
+@app.route('/<int:page_id>/event')
+def event(page_id):
+    s = ''
+    if page_id in urls:
+        s = 'data: %s\r\n\r\n' % urls[page_id]
+    resp = make_response(s)
+    resp.headers['Content-Type'] = 'text/event-stream'
+    return resp
+
+@app.route('/<int:page_id>/manage', methods=['GET', 'POST'])
+def manage(page_id):
+    if request.method == 'POST':
+        urls[page_id] = request.form['url']
+        return redirect(url_for('manage', page_id=page_id))
+    else:
+        return render_template('manage.html',
+                               page_id=page_id,
+                               url=urls.get(page_id, ''))
 
 run_wsgi_app(app)
