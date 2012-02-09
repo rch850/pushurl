69bff8a6fb5f5f82dba4e3dc66a8a59919d22a6d
rch850 <rich850@gmail.com>
2012-02-10 04:24:57 +0900

    Disabled access to page <= 0 or > 10.

--- src/pushurl/views.py
+++ src/pushurl/views.py
@@ -8,6 +8,8 @@ def hello_world():
 
 @app.route('/<int:page_id>')
 def page(page_id):
+    if page_id <= 0 or 10 < page_id:
+        return redirect(url_for('hello_world'))
     return render_template('page.html', page_id=page_id)
 
 @app.route('/<int:page_id>/event')
@@ -22,6 +24,8 @@ def event(page_id):
 
 @app.route('/<int:page_id>/manage', methods=['GET', 'POST'])
 def manage(page_id):
+    if page_id <= 0 or 10 < page_id:
+        return redirect(url_for('hello_world'))
     page = Page.get_by_key_name(str(page_id))
     if request.method == 'POST':
         url = request.form['url']
